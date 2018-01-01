755f017465940c585c2c5d4ef872de9af1a5d6f4
Jiamin Xu <jiamin.xu@live.com>
2018-01-02 00:34:54 +0800

    Add ClAlphaDriver for cfdppwrapper

--- cfdppwrapper.py
+++ cfdppwrapper.py
@@ -3,13 +3,10 @@
 #            File description
 #######################################################
 #  Run a series of CFD++ cases to obtain CL-Alpha property
-#  1. check "istart" and "nstep"
-#  2. 
-#
 #
 #######################################################
 #    Date        Author        Comment
-#  27-Aug-2017   Jiamin Xu     Initial creation
+#  01-Jan-2018   Jiamin Xu     Initial creation
 #######################################################
 #            Import module
 #######################################################
@@ -27,18 +24,14 @@ import shutil
 #            Class
 #######################################################
 class CFDppWrapper:
-    def __init__(self, caseName, numProcs, alpha, isClDriver, ntstep, fromCaseName):
+    def __init__(self, caseName, fromCaseName, ntstep):
         '''Initialize function'''
-        self.caseName = caseName
-        self.fromCaseName = fromCaseName
+        self.caseName = caseName                  ## name of current case
+        self.fromCaseName = fromCaseName          ## name of restart case
         self.logFileName = caseName + ".log"
-        
-        self.numProcs = numProcs
 
-        self.isRestart = True
-        self.isClDriver = isClDriver
+        self.numProcs = -1
         self.ntStep = ntstep
-        self.alpha = alpha
 
         self.velx = 0.0
         self.vely = 0.0
@@ -46,7 +39,87 @@ class CFDppWrapper:
 
         self.prefix = "===" + self.caseName + "=== "
 
+    def Validate(self):
+        status = True
         
+        ## Check number of processors
+        if self.numProcs <= 0:
+            print (self.prefix + "Wrong processor number!!!")
+            return False
+
+        return status
+
+
+    def CreateCasedir(self):
+        '''Copy/Create CFD++ input files'''
+        try:
+            print (self.prefix + "Creating input files for " + self.caseName + " based on " + self.fromCaseName + "...")
+            sourceDir = "./" + self.fromCaseName
+            targetDir = "./" + self.caseName
+            numCopiedFiles = 0
+            filesToCopy = ["nodesin.bin",                            ## basic input
+                           "mcfd.bc",                                ## basic input
+                           "exbcsin.bin",                            ## basic input
+                           "cellsin.bin",                            ## basic input
+                           "npfopts.inp",                            ## basic output settings
+                           "mcfd_metis.graph",                       ## mpi input
+                           "mcpusin.bin."+str(self.numProcs),        ## mpi input
+                           "mcfd.inp",                               ## cfd++ main control input
+                           "cdepsout.bin",                           ## restart input
+                           "infout1f.inp"]                           ## reference values
+
+            if not os.path.exists(sourceDir):
+                return False
+
+            ## create case folder if not exists
+            if not os.path.exists(targetDir):
+                os.makedirs(targetDir)
+            else:
+                shutil.rmtree(targetDir)
+                os.makedirs(targetDir)
+            
+            for sourceFile in os.listdir(sourceDir):
+                if sourceFile in filesToCopy:
+                    sourceFilePath = os.path.join(sourceDir, sourceFile)
+                    targetFilePath = os.path.join(targetDir, sourceFile)            
+                    shutil.copyfile(sourceFilePath, targetFilePath)
+                    numCopiedFiles += 1
+
+            if numCopiedFiles < 10:
+                return False
+        except Exception,e:
+            print e
+            exit(1)
+        else:
+            print self.prefix + str(numCopiedFiles) + " copied into " + self.caseName + "!"
+            return True
+
+        
+    def RunCFDpp(self):
+        '''Execute current CFD++ case'''
+        runCFDpp = "mpiexec -n " + str(self.numProcs) + " mpimcfd > " + self.logFileName
+        try:
+            ret = os.chdir(self.caseName)
+            print self.prefix + runCFDpp
+            ret = os.system(runCFDpp)
+            ret = os.chdir("..")
+        except Exception,e:
+            print e
+            exit(1)
+        else:
+            print self.prefix + "Computation finished!!!"
+
+
+class ClAlphaDriver(CFDppWrapper):
+    def __init__(self, caseName, fromCaseName, ntstep, alpha):
+        CFDppWrapper.__init__(self, caseName, fromCaseName, ntstep)
+        
+        self.alpha = alpha  ## angle of attack
+        self.isRestart = True
+
+        pass
+
+    
     def CreateInpFile(self):
         '''Create mcfd.inp file for current case'''
         try:
@@ -93,8 +166,7 @@ class CFDppWrapper:
                 if "aero_alpha" in inpTexts[i]:
                     inpTexts[i] = "aero_alpha " + str(self.alpha) + "\n"
 
-                if "cldriver " in inpTexts[i] and \
-                   not self.isClDriver:
+                if "cldriver " in inpTexts[i]:
                     inpTexts[i] = "cldriver 0\n"
   
             ## write new mcfd.inp file
@@ -119,95 +191,13 @@ class CFDppWrapper:
 
         except Exception,e:
             print e
-            exit(1)
-
+            exit(1)    
+        
             
-    def __IsClDriver(self):
-        '''Check if base case is a cl-driver case'''
-        isClDriver = False
-        try:          
-            inpFile = open("./" + self.caseName + "/mcfd.inp")
-            inpTexts = inpFile.readlines()
-
-            for i in range(len(inpTexts)):
-                if "cldriver" in inpTexts[i]:
-                    clDriver = int(inpTexts[i].split()[1])
-                    break
-            if clDriver > 0:
-                isClDriver = True
-
-            inpFile.close()
-                    
-        except Exception,e:
-            print e
-            exit(1)
-            
-        return isClDriver
-
-    def CreateCasedir(self):
-        '''Copy/Create CFD++ input files'''
-        try:
-            print self.prefix + "Creating input files for " + self.caseName + " based on " + self.fromCaseName + "..."
-            sourceDir = "./" + self.fromCaseName
-            targetDir = "./" + self.caseName
-            numCopiedFiles = 0
-            filesToCopy = ["nodesin.bin",                            ## basic input
-                           "mcfd.bc",                                ## basic input
-                           "exbcsin.bin",                            ## basic input
-                           "cellsin.bin",                            ## basic input
-                           "npfopts.inp",                            ## basic output settings
-                           "mcfd_metis.graph",                       ## mpi input
-                           "mcpusin.bin."+str(self.numProcs),        ## mpi input
-                           "mcfd.inp",                               ## cfd++ main control input
-                           "cdepsout.bin",                           ## restart input
-                           "infout1f.inp"]                           ## reference values
-
-            if not os.path.exists(sourceDir):
-                return False
-
-            ## create case folder if not exists
-            if not os.path.exists(targetDir):
-                os.makedirs(targetDir)
-            else:
-                shutil.rmtree(targetDir)
-                os.makedirs(targetDir)
-            
-            for sourceFile in os.listdir(sourceDir):
-                if sourceFile in filesToCopy:
-                    sourceFilePath = os.path.join(sourceDir, sourceFile)
-                    targetFilePath = os.path.join(targetDir, sourceFile)            
-                    shutil.copyfile(sourceFilePath, targetFilePath)
-                    numCopiedFiles += 1
-
-            if numCopiedFiles < 10:
-                return False
-        except Exception,e:
-            print e
-            exit(1)
-        else:
-            print self.prefix + str(numCopiedFiles) + " copied into " + self.caseName + "!"
-            return True
-            
-    def RunCFDpp(self):
-        '''Execute current CFD++ case'''
-        runCFDpp = "mpiexec -n " + str(self.numProcs) + " mpimcfd > " + self.logFileName
-        try:
-            ret = os.chdir(self.caseName)
-            print self.prefix + runCFDpp
-            ret = os.system(runCFDpp)
-            ret = os.chdir("..")
-        except Exception,e:
-            print e
-            exit(1)
-        else:
-            print self.prefix + "Computation finished!!!"
                 
 #######################################################
 #            Main Function
 #######################################################
 if __name__ == '__main__':
-    runcfdpp = CFDppWrapper("MA0.85_AOA1.7", 20, 3.5, False, 300, "MA0.85_AOA1.6")
-    runcfdpp.CreateCasedir()
-    runcfdpp.CreateInpFile()
-    #runcfdpp.RunCFDpp()
+
 
